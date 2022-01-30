ea40ae6668a6a8b37f1d95f64ac69b7585c7a128
Ahmad Saleh <ahmadfds@gmail.com>
2022-01-30 14:22:49 +0300

    add annotation

--- src/Contract/IDependencyInjector.php
+++ src/Contract/IDependencyInjector.php
@@ -4,5 +4,10 @@ namespace Jawabkom\Standard\Contract;
 
 interface IDependencyInjector
 {
+    /**
+     * @psalm-template RealInstanceType of object
+     * @psalm-param class-string<RealInstanceType> $type
+     * @psalm-return RealInstanceType
+     */
     public function make(string $type, array $arguments = []):mixed;
 }
